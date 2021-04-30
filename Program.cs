8d67519a633dc03c52b20b75c9783c2c2252598f
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2021-04-30 15:35:11 -0700

    finished off argument input and started neighbor checking

--- Program.cs
+++ Program.cs
@@ -30,21 +30,25 @@ namespace ConwaysGameOfLife
 
             int gridCount = 0;
             int[,] grid = new int[GridSizeX, GridSizeY];
+            int[,] nextGrid = new int[GridSizeX, GridSizeY];
 
             if (args.Length == 0)
             {
-                mode = "i";
+                mode = "interactive";
                 pattern = "R";
+                maxGenerations = -1;
             }
             else if (args.Length == 1)
             {
                 if (args[0] == "interactive" || args[0] == "i")
                 {
-                    mode = "i";
+                    mode = "interactive";
+                    maxGenerations = -1;
                 }
                 else if (args[0] == "silent" || args[0] == "s")
                 {
-                    mode = "s";
+                    mode = "silent";
+                    maxGenerations = 50;
                 }
 
                 pattern = "R";
@@ -53,14 +57,20 @@ namespace ConwaysGameOfLife
             {
                 if (args[0] == "interactive" || args[0] == "i")
                 {
-                    mode = "i";
+                    mode = "interactive";
+                    maxGenerations = -1;
                 }
                 else if (args[0] == "silent" || args[0] == "s")
                 {
-                    mode = "s";
+                    mode = "silent";
+                    maxGenerations = 50;
                 }
 
-                if (int.TryParse(args[1], out fillPercent))
+                if (args[1] == "R")
+                {
+                    pattern = "R";
+                }
+                else if (int.TryParse(args[1], out fillPercent))
                 {
                     if (fillPercent >= 0 && fillPercent <= 100)
                     {
@@ -68,38 +78,106 @@ namespace ConwaysGameOfLife
                     }
                 }
             }
+            else if (args.Length == 3)
+            {
+                if (args[0] == "interactive" || args[0] == "i")
+                {
+                    mode = "interactive";
+                }
+                else if (args[0] == "silent" || args[0] == "s")
+                {
+                    mode = "silent";
+                }
+
+                if (args[1] == "R")
+                {
+                    pattern = "R";
+                }
+                else if (int.TryParse(args[1], out fillPercent))
+                {
+                    if (fillPercent >= 0 && fillPercent <= 100)
+                    {
+                        FillGridRandomly(grid, fillPercent);
+                    }
+                }
+
+                int.TryParse(args[2], out maxGenerations);
+            }
 
             logger.Info("... argument Parsing complete");
+            logger.Info($"{mode} mode");
+            if (pattern == "R")
+            {
+                logger.Info($"starting with: R-Pentamino");
+            }
+            logger.Info($"running for {maxGenerations} Generations");
+            if (pattern == "R")
+            {
+                logger.Info($"--- Filling grid with R-Pentamino pattern");
 
+            }
+            else
+            {
+                logger.Info($"filling grid randomly at {fillPercent} fill percent");
+            }
             bool done = false;
             while (!done)
             {
-                // Randomly Fill the Grid
-                FillGridRandomly(grid, 20);
-
-                // Display the grid (and log its statistics)
-                WriteLine($"Grid #{gridCount}");
-                PrintGrid(grid);
-                logger.Debug($"Grid #{gridCount}  aliveCount: {CountLiveCells(grid)}");
-
-                Thread.Sleep(500);
-
-                // Check to see if the user pressed a key
-                if (Console.KeyAvailable)
+                logger.Info($"Generation: {gridCount}  aliveCount: {CountLiveCells(grid)}");
+                if (mode == "interactive")
                 {
-                    ConsoleKey key = Console.ReadKey(true).Key;
-                    logger.Debug($"{key} pressed...");
-                    if (key == ConsoleKey.Q)
-                        done = true;
-                    else if (key == ConsoleKey.F)
+                    if (Console.KeyAvailable)
                     {
 
-                    }
-                    else if (key == ConsoleKey.R)
-                    {
 
+
+                        Thread.Sleep(500);
+
+                        ConsoleKey key = Console.ReadKey(true).Key;
+                        logger.Debug($"{key} pressed...");
+                        if (key == ConsoleKey.Q)
+                            done = true;
+                        else if (key == ConsoleKey.F)
+                        {
+
+                        }
+                        else if (key == ConsoleKey.R)
+                        {
+
+                        }
                     }
                 }
+                else if (mode == "silent")
+                {
+
+                }
+
+                // Randomly Fill the Grid
+                // FillGridRandomly(grid, 20);
+
+                // Display the grid (and log its statistics)
+                // WriteLine($"Grid #{gridCount}");
+                // PrintGrid(grid);
+                // logger.Debug($"Grid #{gridCount}  aliveCount: {CountLiveCells(grid)}");
+
+                // Thread.Sleep(500);
+
+                // Check to see if the user pressed a key
+                // if (Console.KeyAvailable)
+                // {
+                //     ConsoleKey key = Console.ReadKey(true).Key;
+                //     logger.Debug($"{key} pressed...");
+                //     if (key == ConsoleKey.Q)
+                //         done = true;
+                //     else if (key == ConsoleKey.F)
+                //     {
+
+                //     }
+                //     else if (key == ConsoleKey.R)
+                //     {
+
+                //     }
+                // }
 
                 gridCount++;            // Increment at bottom of loop so that first grid displayed is Grid #0
             }
@@ -156,6 +234,59 @@ namespace ConwaysGameOfLife
                     if (grid[x, y] == Alive)
                         count++;
             return count;
+        }
+
+        static void checkNeighbors(int[,] currentGrid, int[,] nextGrid)
+        {
+            for (int x = 0; x < GridSizeX; x++)
+            {
+                for (int y = 0; y < GridSizeY; y++)
+                {
+                    int[,] neighbors = new int[3, 3];
+
+                    if (x - 1 > 0)
+                    {
+                        if (y - 1 > 0)
+                        {
+                            neighbors[0, 0] = currentGrid[x - 1, y - 1];
+                        }
+
+                        if (y + 1 < GridSizeY)
+                        {
+                            neighbors[0, 2] = currentGrid[x - 1, y + 1];
+                        }
+
+                        neighbors[0, 1] = currentGrid[x - 1, y];
+                    }
+
+                    if (x + 1 < GridSizeX)
+                    {
+                        if (y - 1 > 0)
+                        {
+                            neighbors[2, 0] = currentGrid[x + 1, y - 1];
+                        }
+
+                        if (y + 1 < GridSizeY)
+                        {
+                            neighbors[2, 2] = currentGrid[x + 1, y + 1];
+                        }
+
+                        neighbors[2, 1] = currentGrid[x + 1, y];
+                    }
+
+                    if (y - 1 > 0)
+                    {
+                        neighbors[1, 0] = currentGrid[x, y - 1];
+                    }
+
+                    if (y + 1 < GridSizeY)
+                    {
+                        neighbors[1, 2] = currentGrid[x, y + 1];
+                    }
+
+                    neighbors[1, 1] = currentGrid[x, y];
+                }
+            }
         }
     }
 }
